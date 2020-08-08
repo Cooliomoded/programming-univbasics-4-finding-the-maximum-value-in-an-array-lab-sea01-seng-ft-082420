c2a8d26349bd544bf6ea3ed9296f97d9db58f4d9
Davis Brown <cooliomoded@gmail.com>
2020-08-08 01:17:22 +0000

    Automatically backed up by Learn

--- lib/finding_the_max_value.rb
+++ lib/finding_the_max_value.rb
@@ -3,7 +3,7 @@ def find_max_value(array)
   while i < array.length do
     if array[i - 1] >= array[i] == true
     array.delete[i]
-  else array.delete[i - 1]
+  else array.delete([i - 1])
     end
   end
   puts array
